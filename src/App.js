227867707b4b2daa30d0de37bed3ad143fa2a0df
cjay-91 <109164936+cjay-91@users.noreply.github.com>
2023-07-02 19:57:22 +0530

    added 4 columns

--- src/App.js
+++ src/App.js
@@ -4,22 +4,20 @@ import {DataTable} from "primereact/datatable";
 import {Column} from "primereact/column";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css"; 
+import fakedata from "./data1.json";
+import React from 'react';
 
 function App() {
 
-  const data = [
-    {id:1, name: 'John', age: 20, city: 'New York'}, 
-    {id:2, name: 'Mary', age: 25, city: 'Paris'},
-    {id:3, name: 'Peter', age: 30, city: 'London'},
-  ];
+  const data = React.useMemo(() => fakedata, []);
 
   return (
      <div className="App">
       <DataTable value={data} sortMode='multiple'>
-        <Column field="id" header="ID" sortable></Column>
-        <Column field="name" header="Name" sortable></Column>
-        <Column field="age" header="Age" sortable></Column>
-        <Column field="city" header="City" sortable></Column>
+        <Column field="document_title" header="document_title" sortable></Column>
+        <Column field="short_answer" header="short_answer" sortable></Column>
+        <Column field="question_text" header="question_text" sortable></Column>
+        <Column field="yes_no_answer" header="yes_no_answer" sortable></Column>
       </DataTable>
     </div>
   );
