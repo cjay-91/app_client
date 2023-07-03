3e62b01f611855d4231c860ca3b5fa46af493416
cjay-91 <109164936+cjay-91@users.noreply.github.com>
2023-07-03 06:26:24 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -17,6 +17,18 @@ function App() {
         <Column field="document_title" header="document_title" sortable></Column>
         <Column field="question_text" header="question_text" sortable></Column>
         <Column field="0" header="short_answers" sortable></Column>
+        <Column field="1" header="short_answers" sortable></Column>
+        <Column field="2" header="short_answers" sortable></Column>
+        <Column field="3" header="short_answers" sortable></Column>
+        <Column field="4" header="short_answers" sortable></Column>
+        <Column field="5" header="short_answers" sortable></Column>
+        <Column field="6" header="short_answers" sortable></Column>
+        <Column field="7" header="short_answers" sortable></Column>
+        <Column field="8" header="short_answers" sortable></Column>
+        <Column field="9" header="short_answers" sortable></Column>
+        <Column field="10" header="short_answers" sortable></Column>
+        <Column field="11" header="short_answers" sortable></Column>
+        <Column field="12" header="short_answers" sortable></Column>
         <Column field="yes_no_answer" header="yes_no_answer" sortable></Column>
       </DataTable>
     </div>
